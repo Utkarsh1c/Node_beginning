ff739a75b39768930f14f736730449b558c18cf1
Utkarsh Mittal <umutkarsh@gmail.com>
2019-09-01 13:29:27 +0530

    basic routing in node

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 const http = require('http');
+const routes = require('./routes');
 
-// function rqListener(req, res) {
+console.log(routes.someText);
 
-// }
-
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
-    res.setHeader('Content-Type', 'text.html');
-    res.write('<html>');
-    res.write('<head>My First Page</head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
