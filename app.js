e992df7fc33392c66cc6511e15ee547c24ff9683
Utkarsh Mittal <umutkarsh@gmail.com>
2019-09-02 16:47:46 +0530

    routing through express

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware!");
-    next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req, res, next) => {
-    console.log("In another middleware!");
-    res.send('<h1>Hello from Express!</h1>');
-});
+// app.use((req, res, next) => {
+//     console.log("In the middleware!");
+//     next();
+// });
 
-const server = http.createServer(app);
+app.use(express.urlencoded({extended: false}));
 
-server.listen(3000);
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+
+
+app.listen(3000);
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
